ea7f35280b8018435eef6c3144526bdc5565a393
vovador <vovadorko@gmail.com>
2024-09-20 14:17:12 +0000

    project boostrap

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl ma-auto">
+      <h1 className="text-2xl text-white underline"> Hello, Three.js</h1>
+    </main>
+  );
+};
