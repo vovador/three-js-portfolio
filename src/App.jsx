11de90a95307989cdf72f275e85a9ee776f4a169
vovador <vovadorko@gmail.com>
2024-09-20 17:48:41 +0000

    feat: prototype solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import Hero from "./sections/Hero.jsx";
+import About from "./sections/About.jsx";
+import Footer from "./sections/Footer.jsx";
+import Navbar from "./sections/Navbar.jsx";
+import Contact from "./sections/Contact.jsx";
+import Clients from "./sections/Clients.jsx";
+import Projects from "./sections/Projects.jsx";
+import WorkExperience from "./sections/Experience.jsx";
 
 const App = () => {
   return (
-    <main className="max-w-7xl ma-auto">
-      <h1 className="text-2xl text-white underline"> Hello, Three.js</h1>
+    <main className="max-w-7xl mx-auto relative">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <WorkExperience />
+      <Contact />
+      <Footer />
     </main>
   );
 };
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,167 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Work",
+    href: "#work",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: "Emily Johnson",
+    position: "Marketing Director at GreenLeaf",
+    img: "assets/review1.png",
+    review:
+      "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+  },
+  {
+    id: 2,
+    name: "Mark Rogers",
+    position: "Founder of TechGear Shop",
+    img: "assets/review2.png",
+    review:
+      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.",
+  },
+  {
+    id: 3,
+    name: "John Dohsas",
+    position: "Project Manager at UrbanTech ",
+    img: "assets/review3.png",
+    review:
+      "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+  },
+  {
+    id: 4,
+    name: "Ether Smith",
+    position: "CEO of BrightStar Enterprises",
+    img: "assets/review4.png",
+    review:
+      "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.",
+  },
+];
+
+export const myProjects = [
+  {
+    title: "Org Chart",
+    desc: "Org Chart App provides the ability to represent relations between the board items in interactive, dynamic, and collaborative org chart view.",
+    subdesc:
+      "Easily rearrange items in your org chart view using a simple drag-and-drop interface. Change connections or establish new relations just in a few simple clicks.",
+    href: "https://monday.com/marketplace/listing/10000312/org-chart",
+    texture: "/textures/project/project1.mp4",
+    logo: "/assets/project-logo1.png",
+    logoStyle: {
+      backgroundColor: "#2A1816",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
+    },
+    spotlight: "/assets/spotlight1.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+  {
+    title: "Resynced: Seamless Two-Way Sync for Your Data",
+    desc: "Resynced offers a powerful and intuitive solution for synchronizing your data between platforms like monday.com, Google Sheets, and Notion",
+    subdesc:
+      "With fine-tuned control over sync settings, including key column matching and column mappings, Resynced ensures your data stays consistent, organized, and up-to-date across all your tools.",
+    href: "https://app.resynced.io/",
+    texture: "/textures/project/project2.mp4",
+    logo: "/assets/project-logo2.png",
+    logoStyle: {
+      backgroundColor: "#13202F",
+      border: "0.2px solid #17293E",
+      boxShadow: "0px 0px 60px 0px #2F6DB54D",
+    },
+    spotlight: "/assets/spotlight2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: "LeanyLabs",
+    pos: "Full-Stack Developer",
+    duration: "March 2021 - September 2024",
+    title:
+      "In this role, I was responsible for the end-to-end development of CRM automation applications. My responsibilities included: Business Logic Development, Security, Automated Testing, Deployment",
+    icon: "/assets/leanylabs.png",
+    animation: "clapping",
+  },
+  {
+    id: 2,
+    name: "QuantumPixel",
+    pos: "Junior Web Developer",
+    duration: "February 2020 - March 2021",
+    title: "I was responsible for the continuous maintenance, optimization, and smooth operation of a dynamic website",
+    icon: "/assets/quantumpixel.jpg",
+    animation: "salute",
+  },
+];
